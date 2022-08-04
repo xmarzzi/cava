d134c64ecf6793401e934c8ee53c4a02bfaa191c
Xavier Martinuzzi <xaviermagg@gmail.com>
2022-08-04 02:35:08 -0300

    formulario

--- pages/form.php
+++ pages/form.php
@@ -4,14 +4,14 @@ $telefono = $_POST ['celular'];
 $email = $_POST ['correo'];
 $mensaje = $_POST ['mensaje'];
 
-$mensaje = "Este mensaje fue enviado por ". $nombre . ".\r\n";
-$mensaje .= "Su telefono es: ". $telefono . ".\r\n";
-$mensaje .= "Su e-mail es: ". $email . "\r\n";
-$mensaje .= "Mensaje: ". $_POST ['mensaje'] . "\r\n";
+$mensaje = "Este mensaje fue enviado por " $nombre  "\r\n";
+$mensaje = "Su telefono es: " $telefono  "\r\n";
+$mensaje = "Su e-mail es: " $email  "\r\n";
+$mensaje = "Mensaje: " $_POST ['mensaje']  "\r\n";
 
 $destinatario = "xavier.martinuzzi@gmail.com";
 $asunto = "Este mail fue enviado desde la web CAVA";
 
-mail ($destinatario, $asunto, utf8_decode($mensaje), $header);
+mail ($destinatario, $asunto,$mensaje);
 header('Location:exito.html');
 ?>
