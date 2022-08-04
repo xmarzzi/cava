89880d74e0f1afe2dfd9e6b24d748af24217c9f0
Xavier Martinuzzi <xaviermagg@gmail.com>
2022-08-04 02:14:10 -0300

    formulario y página de exito

---
+++ pages/form.php
@@ -0,0 +1,17 @@
+<?php
+$nombre = $_POST ['nombre'];
+$telefono = $_POST ['celular'];
+$email = $_POST ['correo'];
+$mensaje = $_POST ['mensaje'];
+
+$mensaje = "Este mensaje fue enviado por ". $nombre . ".\r\n";
+$mensaje .= "Su telefono es: ". $telefono . ".\r\n";
+$mensaje .= "Su e-mail es: ". $email . "\r\n";
+$mensaje .= "Mensaje: ". $_POST ['mensaje'] . "\r\n";
+
+$destinatario = "xavier.martinuzzi@gmail.com";
+$asunto = "Este mail fue enviado desde la web CAVA";
+
+mail ($destinatario, $asunto, utf8_decode($mensaje), $header);
+header('Location:exito.html');
+?>
